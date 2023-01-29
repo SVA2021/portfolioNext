15503d89bf704b90a1d5ace9da9fecc9087d4821
SVA2021 <sofronov.vit@gmail.com>
2023-01-29 13:59:25 +0400

    add env links for cv

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    hrefEngCV: './Sofronov_Vitaliy_FrontEnd_Developer_170123.pdf',
+    hrefRuCV: './FrontEnd_Developer_170123.pdf',
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
