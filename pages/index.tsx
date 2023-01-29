0b87c4a5770f7be4f2f99a91caf28676fb280ada
SVA2021 <sofronov.vit@gmail.com>
2023-01-29 20:09:17 +0400

    homepage initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,59 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import {Inter} from '@next/font/google'
-import styles from '@/styles/Home.module.scss'
-import {Layout} from '@/components'
+import {HeroLink, HeroWord} from '@/components'
+import {LangContext} from '@/contexts'
+import {Layout} from '@/layout'
+import s from '@/styles/Home.module.scss'
+import {useContext} from 'react'
 
-const inter = Inter({subsets: ['latin']})
+const me = {
+  en: {fullName: ['Sofronov', 'Vitaliy', 'Alexandrovich'], },
+  ru: {fullName: ['Софронов', 'Виталий', 'Александрович'], },
+}
+
+const HeroLinks = [
+  {
+    title: 'telegram',
+    href: process.env.href_telegram,
+  },
+  {
+    title: 'discord',
+    href: process.env.href_discord,
+  },
+  {
+    title: 'github',
+    href: process.env.href_github,
+  },
+  {
+    title: 'hh',
+    href: process.env.href_hh,
+  },
+]
 
 export default function Home() {
+
+  const {lang} = useContext(LangContext);
+  const fullName = me[lang].fullName;
+
   return (
     <Layout>
-      some test text
+      <section className={s.hero}>
+        <h1 className={s.hero__name}>
+          {
+            fullName.map((name) =>
+              <HeroWord name={name} key={name} />
+            )
+          }
+        </h1>
+        <p className={s.hero__profession}>Front-End Developer</p>
+        <ul className={s.hero__social}>
+          {
+            HeroLinks.map((item, index) =>
+              <li className={s.hero__item} key={index}>
+                <HeroLink title={item.title} href={item.href ?? ''} />
+              </li>
+            )
+          }
+        </ul>
+      </section>
     </Layout>
   )
-}
+};
