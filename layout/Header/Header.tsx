7f41568e43ddd5dfc0433589592c2dc65fc72467
SVA2021 <sofronov.vit@gmail.com>
2023-01-29 13:59:45 +0400

    header finished

---
+++ layout/Header/Header.tsx
@@ -0,0 +1,12 @@
+import {Logo, NavMenu} from '@/components';
+import {FC} from "react";
+import s from './Header.module.scss';
+
+export const Header: FC = () => {
+  return (
+    <header className={s.header}>
+      <Logo name={'sva'} />
+      <NavMenu />
+    </header>
+  )
+};
