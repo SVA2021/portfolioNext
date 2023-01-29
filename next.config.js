fa0dcc2afff8cb26d26c8924a9af8a2a5265baf7
SVA2021 <sofronov.vit@gmail.com>
2023-01-29 20:07:35 +0400

    add links to main profiles

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    hrefEngCV: './Sofronov_Vitaliy_FrontEnd_Developer_170123.pdf',
-    hrefRuCV: './FrontEnd_Developer_170123.pdf',
+    href_cv_eng: './Sofronov_Vitaliy_FrontEnd_Developer_170123.pdf',
+    href_cv_ru: './FrontEnd_Developer_170123.pdf',
+    href_discord: 'https://discordapp.com/users/842765911681400894',
+    href_github: 'https://github.com/SVA2021',
+    href_telegram: 'https://t.me/SVA_1985',
+    href_hh: 'https://hh.ru/applicant/resumes/view?resume=e30eb6d2ff0983faee0039ed1f62784758716e',
   },
 }
 
