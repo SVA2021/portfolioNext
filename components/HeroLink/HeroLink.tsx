461475b59dc9e61cc01acaba95ced22d53a2836f
SVA2021 <sofronov.vit@gmail.com>
2023-01-29 20:08:15 +0400

    add HeroLink component

---
+++ components/HeroLink/HeroLink.tsx
@@ -0,0 +1,23 @@
+import s from '@/styles/Home.module.scss';
+import {FC} from 'react';
+
+export interface HeroLinkProps {
+  title: string
+  href: string
+}
+
+export const HeroLink: FC<HeroLinkProps> = ({title, href}) => {
+  const svgHref = `/social.svg#${title}`;
+  return (
+    <a
+      target="_blank" rel="noreferrer noopener"
+      className={s.hero__link}
+      href={href}
+    >
+      <svg className={s.icon__svg} role="img" aria-label={title}>
+        <use href={svgHref}></use>
+      </svg>
+      <span className={s.hero__caption}>{title}</span>
+    </a>
+  );
+};
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export * from './SvgIcon/SvgIcon';
 export * from './Logo/Logo';
 export * from './NavMenu/NavMenu';
 export * from './HeroWord/HeroWord';
+export * from './HeroLink/HeroLink';
