e3524340af96b878b12c0c17fcb8b8ca318f5fee
SVA2021 <sofronov.vit@gmail.com>
2023-03-15 15:51:55 +0400

    add home page placeholder

---
+++ db/home.ts
@@ -0,0 +1,17 @@
+export const content = {
+  en: {
+    me: ['Sofronov, Vitaliy, Alexandrovich'],
+    cv: {href: process.env.hrefEngCV, title: 'cv'},
+    lang: 'ru',
+  },
+  ru: {
+    menu: [
+      {href: '/', title: 'домой', },
+      {href: '/projects', title: 'Проекты', },
+      {href: '/about', title: 'Обо мне', },
+      {href: '/contacts', title: 'Контакты', },
+    ],
+    cv: {href: process.env.hrefRuCV, title: 'резюме'},
+    lang: 'eng',
+  },
+}
