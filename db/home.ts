b715f638e437ad1b4d7c335f8cfcb2e322421b7e
SVA2021 <sofronov.vit@gmail.com>
2023-03-17 11:05:11 +0400

    add data for home page

--- db/home.ts
+++ db/home.ts
@@ -1,19 +1,22 @@
-export const content = {
-  en: {
-    me: ['Sofronov, Vitaliy, Alexandrovich'],
-    cv: {href: process.env.hrefEngCV, title: 'cv'},
-    lang: 'ru',
+export const HeroLinks = [
+  {
+    src: 'assets/social.svg',
+    id: 'telegram',
+    href: 'https://t.me/SVA_1985',
   },
-  ru: {
-    menu: [
-      {href: '/', title: 'домой', },
-      {href: '/projects', title: 'Проекты', },
-      {href: '/about', title: 'Обо мне', },
-      {href: '/contacts', title: 'Контакты', },
-    ],
-    cv: {href: process.env.hrefRuCV, title: 'резюме'},
-    lang: 'eng',
+  {
+    src: 'assets/social.svg',
+    id: 'discord',
+    href: 'https://discordapp.com/users/842765911681400894',
   },
-}
-
-//todo move all to locales
+  {
+    src: 'assets/social.svg',
+    id: 'github',
+    href: 'https://github.com/SVA2021',
+  },
+  {
+    src: 'assets/social.svg',
+    id: 'hh',
+    href: 'https://hh.ru/applicant/resumes/view?resume=e30eb6d2ff0983faee0039ed1f62784758716e',
+  },
+]
