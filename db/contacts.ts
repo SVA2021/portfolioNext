89f7f594a9743b983f7d4fc079afa8e322053474
SVA2021 <sofronov.vit@gmail.com>
2023-03-19 11:24:57 +0400

    add contacts page initial layout

---
+++ db/contacts.ts
@@ -0,0 +1,34 @@
+export const CONTACTS_LINKS = [
+    {
+        type: 'telegram',
+        data: 'SVA_1985',
+        href: 'https://t.me/SVA_1985'
+    },
+    {
+        type: 'discord',
+        data: 'Vitaliy S.',
+        href: 'https://discordapp.com/users/842765911681400894'
+    },
+    {
+        type: 'github',
+        data: 'SVA2021',
+        href: 'https://github.com/SVA2021'
+    },
+    {
+        type: 'viber',
+        data: '+79966236425',
+        href: 'viber://chat?number=%2B79966236425'
+    },
+    {
+        type: 'phone',
+        data: '+79966236425',
+        href: 'tel:+79966236425'
+    },
+    {
+        type: 'email',
+        data: 'sofronov.vit@gmail.com',
+        href: 'mailto:sofronov.vit@gmail.com'
+    },
+];
+
+export const svgIconSrc  = 'social.svg';
---
+++ pages/contacts.tsx
@@ -0,0 +1,50 @@
+import {Layout} from '@/layout'
+import {SvgIcon, Title} from "@/components";
+import {GetStaticProps} from "next";
+import {serverSideTranslations} from "next-i18next/serverSideTranslations";
+import {useTranslation} from "next-i18next";
+import s from '@/styles/Contacts.module.scss';
+import {CONTACTS_LINKS, svgIconSrc} from "@/db/contacts";
+
+export default function Contacts() {
+
+    const {t} = useTranslation('contacts');
+
+    return (
+        <Layout>
+            <section className="contacts">
+                <Title text={t('title')} type={"h2"}/>
+                <div className={s.contacts__inner}>
+                    <div className={s.contacts__form}>
+                        form will be here
+                    </div>
+                    <div className={s.contacts__links}>
+                        <ul className={s.social__body}>
+                            {
+                                CONTACTS_LINKS.map((item) =>
+                                    <li key={item.type} className={s.social__item}>
+                                        <a className={s.social__link} target={"_blank"} rel={"noreferrer"}>
+                                            <SvgIcon src={svgIconSrc} id={item.type} small/>
+                                            <p className={s.social__title}>{item.type}</p>
+                                            <p className={s.social__data}>{item.data}</p>
+                                        </a>
+                                    </li>
+                                )
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+    )
+};
+
+type Props = {};
+export const getStaticProps: GetStaticProps<Props> = async ({locale}) => ({
+    props: {
+        ...(await serverSideTranslations(locale ?? 'en', [
+            'common',
+            'contacts',
+        ])),
+    },
+})
