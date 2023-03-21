efb625333133cb6c8546e8ef5c96e6e229554dcb
SVA2021 <sofronov.vit@gmail.com>
2023-03-21 10:52:37 +0400

    add projects types

---
+++ @types/projects.ts
@@ -0,0 +1,35 @@
+import {baseLangT} from "@/@types/lang";
+
+export type projectMode = 'future' | 'actual';
+export type editMode = 'old' | 'new';
+
+export const WEB_SKILLS_FULL = [
+    'html5', 'css3', 'js', 'git', 'markdown', 'cssModules', 'github', 'react', 'redux', 'sass', 'typescript', 'angular',
+    'nextjs', 'vue', 'graphql',
+] as const;
+
+export  type webSkill = typeof WEB_SKILLS_FULL[number];
+
+export type baseProjectT = {
+    id?: number | string
+    name: string
+    technologies: string
+    why: baseLangT
+    description: baseLangT
+}
+
+export  type futureProjectT = baseProjectT & {
+    newSkills: string
+}
+
+export  type actualProjectT = baseProjectT & {
+    additional: baseLangT
+    gallery: string[]
+    skills: webSkill[]
+    links: linkT[]
+}
+
+export type linkT = {
+    href: string
+    title: baseLangT
+}
