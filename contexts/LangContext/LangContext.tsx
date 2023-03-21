2cbedb2edae395846b8db05050e1f89252da1a1c
SVA2021 <sofronov.vit@gmail.com>
2023-03-21 13:50:25 +0400

    comment unused code

--- contexts/LangContext/LangContext.tsx
+++ contexts/LangContext/LangContext.tsx
@@ -1,40 +1,41 @@
-import React, {FC, ReactNode, useEffect, useState} from 'react';
-
-type EnOrRu = 'en' | 'ru';
-
-type Tlang = {
-  lang: EnOrRu,
-  toggleLang: () => void
-};
-
-type withChildren = {
-  children: ReactNode | ReactNode[] | undefined | null
-}
-
-const defaultLangValue: Tlang = {
-  lang: 'en',
-  toggleLang: () => console.log('language changed'),
-}
-
-export const LangContext = React.createContext<Tlang>(defaultLangValue);
-
-export const LangProvider: FC<withChildren> = ({children}) => {
-  const [lang, setLang] = useState<EnOrRu>('en');
-
-  useEffect(() => {
-    const localLang = window.localStorage.getItem('lang') ?? '';
-    if (localLang === 'en' || localLang === 'ru') setLang(localLang);
-  }, [])
-
-  const toggleLang = () => {
-    const newValue: EnOrRu = lang === 'en' ? 'ru' : 'en';
-    setLang(newValue);
-    window.localStorage.setItem('lang', newValue);
-  };
-
-  return (
-    <LangContext.Provider value={{lang, toggleLang}}>
-      {children}
-    </LangContext.Provider>
-  );
-};
+// import React, {FC, ReactNode, useEffect, useState} from 'react';
+//
+// type EnOrRu = 'en' | 'ru';
+//
+// type Tlang = {
+//   lang: EnOrRu,
+//   toggleLang: () => void
+// };
+//
+// type withChildren = {
+//   children: ReactNode | ReactNode[] | undefined | null
+// }
+//
+// const defaultLangValue: Tlang = {
+//   lang: 'en',
+//   toggleLang: () => console.log('language changed'),
+// }
+//
+// export const LangContext = React.createContext<Tlang>(defaultLangValue);
+//
+// export const LangProvider: FC<withChildren> = ({children}) => {
+//   const [lang, setLang] = useState<EnOrRu>('en');
+//
+//   useEffect(() => {
+//     const localLang = window.localStorage.getItem('lang') ?? '';
+//     if (localLang === 'en' || localLang === 'ru') setLang(localLang);
+//   }, [])
+//
+//   const toggleLang = () => {
+//     const newValue: EnOrRu = lang === 'en' ? 'ru' : 'en';
+//     setLang(newValue);
+//     window.localStorage.setItem('lang', newValue);
+//   };
+//
+//   return (
+//     <LangContext.Provider value={{lang, toggleLang}}>
+//       {children}
+//     </LangContext.Provider>
+//   );
+// };
+export {}
