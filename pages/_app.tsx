089fb220e7b5065740b7730c91cf3c2222706c66
SVA2021 <sofronov.vit@gmail.com>
2023-03-21 10:55:58 +0400

    add auth context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '@/styles/globals.scss'
 import type {AppProps} from 'next/app'
 import {appWithTranslation} from 'next-i18next'
+import {AuthContextProvider} from "@/contexts/Auth/AuthContext";
 
-const MyApp = ({Component, pageProps}: AppProps) => <Component {...pageProps} />
+const MyApp = ({Component, pageProps}: AppProps) => {
+    return (
+        <AuthContextProvider>
+            <Component {...pageProps} />
+        </AuthContextProvider>
+    )
+}
 
 export default appWithTranslation(MyApp);
